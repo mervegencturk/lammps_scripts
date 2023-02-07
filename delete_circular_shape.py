99804716b4922570cb855f4c81675942ac5ea0f2
mervegencturk <118142159+mervegencturk@users.noreply.github.com>
2023-02-07 11:59:20 -0600

    Create delete_circular_shape.py
    In this script, the condition inside the loop has been changed to check if the position of the current atom is outside the circle with the specified radius.

---
+++ delete_circular_shape.py
@@ -0,0 +1,39 @@
+def delete_atoms(file_name, radius):
+    # Open the file
+    with open(file_name, "r") as f:
+        lines = f.readlines()
+
+    # Get the number of atoms
+    n_atoms = int(lines[3].strip().split()[0])
+
+    # Initialize a list to store the indices of atoms to be deleted
+    delete_list = []
+
+    # Loop through the atoms to identify the ones to be deleted
+    for i in range(n_atoms):
+        # Get the current atom's position
+        x, y, z = map(float, lines[9 + i].strip().split()[2:5])
+
+        # Check if the position is outside the circle
+        if x**2 + y**2 > radius**2:
+            delete_list.append(i + 1)
+
+    # Create a new file to store the results
+    with open("new_structure.data", "w") as f:
+        # Copy the header lines from the original file
+        for line in lines[:9]:
+            f.write(line)
+
+        # Loop through the atoms
+        for i in range(n_atoms):
+            # Check if the current atom is not in the delete list
+            if i + 1 not in delete_list:
+                f.write(lines[9 + i])
+
+        # Update the number of atoms in the header
+        n_atoms -= len(delete_list)
+        f.seek(0)
+        f.write(str(n_atoms) + "\n\n")
+
+# Example usage
+delete_atoms("structure.data", 1.0)
